954d85b2da8bd0a80b803f3b6979916a0b5598bf
Thirupura Sundari Jayakumar <TJayakumar@corp.encoress.com>
2018-09-03 16:56:19 +0530

    code

---
+++ calc.java
@@ -0,0 +1,29 @@
+       
+       
+       
+       
+       //Prime Number program:wq
+
+
+       
+       public class Prime
+       {
+       public static void main(String[] args)
+       {
+        int num;
+	for(int i=1;i<=100;i++)
+	{
+	int count=0;
+	for(int j=2;j<i;j++)
+	{
+	if(i%j==0)
+	{
+	count++;
+	}
+																									}
+																										if(count==0)
+																										System.out.print(i+" ");
+																									}
+																									}	
+																									}
+																														
