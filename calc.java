41b6e94004a86a14518dcdc51d57feabee39120a
Thirupura18 <39131068+Thirupura18@users.noreply.github.com>
2018-09-04 10:30:28 +0530

    Revert "Code is properly aligned"

--- calc.java
+++ calc.java
@@ -1,20 +1,26 @@
+       
+       
+       
+       
        //Prime Number program       
        public class Prime
        {
-            public static void main(String[] args)
-            {
-                int num;
-	        for(int i=1;i<=100;i++)
-	        {
-	           int count=0;
-	           for(int j=2;j<i;j++)
-	           {
-	                 if(i%j==0)
-	                 {
-	                   count++;
-	                 }																						           }																							        if(count==0)	
-	          System.out.print(i+" ");
-		}
-																								             }			
-																								          }
+       public static void main(String[] args)
+       {
+        int num;
+	for(int i=1;i<=100;i++)
+	{
+	int count=0;
+	for(int j=2;j<i;j++)
+	{
+	if(i%j==0)
+	{
+	count++;
+	}
+																									}
+																									if(count==0)
+																									System.out.print(i+" ");
+																									}
+																									}	
+																									}
 																														
