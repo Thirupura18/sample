40a7874e860cd1990f3542b90317be8f3df24882
Saranya Selvarasu <SSelvarasu@corp.encoress.com>
2018-09-04 16:22:44 +0530

    calc added

--- calc.java
+++ calc.java
@@ -1,26 +1,14 @@
-       
-       
-       
-       
-       //Prime Number program       
-       public class Prime
-       {
-       public static void main(String[] args)
-       {
-        int num;
-	for(int i=1;i<=100;i++)
-	{
-	int count=0;
-	for(int j=2;j<i;j++)
-	{
-	if(i%j==0)
-	{
-	count++;
-	}
-																									}
-																									if(count==0)
-																									System.out.print(i+" ");
-																									}
-																									}	
-																									}
-																														
+
+
+
+public class Calc
+{
+    public static void main(String args[])
+    {
+            int a=10,b=30,c;
+	    c=a+b;
+	    Syatem.out.println("the value of c:"+c);
+     }
+}     
+
+
